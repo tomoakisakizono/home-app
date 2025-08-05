e02add080cf26ead9387dc9460fa75fa345d374f
tomoakisakizono <123532464+tomoakisakizono@users.noreply.github.com>
2025-08-05 15:25:02 +0900

    Revert "♻️ code-adjustments のRevert対策再適用"

--- src/app/Models/Message.php
+++ src/app/Models/Message.php
@@ -9,15 +9,25 @@ class Message extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'pair_id', 'content', 'scheduled_at'];
+    protected $fillable = ['content', 'scheduled_at', 'family_id', 'receiver_id', 'is_read', 'sender_id'];
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'sender_id');
     }
 
-    public function pair()
+    public function family()
     {
-        return $this->belongsTo(Pair::class);
+        return $this->belongsTo(Family::class);
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
     }
 }
